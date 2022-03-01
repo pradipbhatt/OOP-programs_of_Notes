5540c88d2e84042ca9219f3739ac074c371adca5
pradip bhatt <84025490+pradipbhatt@users.noreply.github.com>
2022-03-01 13:39:49 +0545

    Create setw(field_weidth).cpp

---
+++ setw(field_weidth).cpp
@@ -0,0 +1,11 @@
+#include <iostream> // std::cout, std::endl
+#include <iomanip>  // std::setw
+using namespace std;
+
+int main()
+{
+    system("cls");
+    cout << setw(20);    // setw manipulator helps to format the display of output here outout comes 20 place right ....
+    cout << 24 << endl;
+    return 0;
+}
