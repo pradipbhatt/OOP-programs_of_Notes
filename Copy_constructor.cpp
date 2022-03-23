64d7d66aa715abc49a06873c57b5e6f3aa1b2d40
pradip bhatt <84025490+pradipbhatt@users.noreply.github.com>
2022-03-23 20:46:03 +0545

    Create Copy_constructor.cpp

---
+++ Copy_constructor.cpp
@@ -0,0 +1,35 @@
+//WAP for copy parameterized and default constructor...
+#include<iostream>
+using namespace std;
+class student
+{
+int id;
+public:
+student(){}                //default constructor is called here..
+student(int a)             //parameterized constructor is defined here...
+{
+    id=a;
+}
+student(student &s)         //object ko adress lae as a argument pass garyo so,here copy constructor is called...(&s=s1=200)
+{
+    id=s.id;
+}
+int display()
+{
+    return(id);
+}
+};
+
+int main()
+{
+    system("cls");
+    student s1(200);             //s1 object le parameterized constructor student lae call garyoo and 200 pass garyoo...
+    student s2(s1);              //s2=s1 copy constructor student is called here and s2 is
+    student s3(s2);
+    student s4(s1);
+    cout<<"Id of student s1="<<s1.display()<<endl;
+    cout<<"Id of student s1="<<s2.display()<<endl;
+    cout<<"Id of student s1="<<s3.display()<<endl;
+    cout<<"Id of student s1="<<s4.display()<<endl;
+
+}
